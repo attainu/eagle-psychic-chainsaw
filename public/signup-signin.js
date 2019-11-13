cdf5832a18f42c6c4d6852bfccd35e240375ece3
nishekh <nishekhgowda143@gmail.com>
2019-11-14 00:30:59 +0530

    user/seller signin&signup

---
+++ public/signup-signin.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function () {
+    $('#signUp').on('click',function(){
+        $('#container').addClass('right-panel-active');
+    });
+    $('#signIn').on('click',function(){
+        $('#container').removeClass('right-panel-active');
+    });
+});
