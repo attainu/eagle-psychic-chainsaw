3e9135dfc2f5ae17a1c4e62f71917137ad661c3a
SITANSHU KUMAR <41771170+sitanshu-kumar-vst-au4@users.noreply.github.com>
2019-11-15 18:46:15 +0530

    modified

---
+++ feature_sellerProfile/controllers/e-commerce.js
@@ -0,0 +1,126 @@
+const Controller = {};
+const model = require('./../models/E-Commerce.js');
+
+// Home route
+Controller.home = function (req, res) {
+    var random = null;
+    model.home(random, function (error, info) {
+        return res.render('homepage', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+
+// Product List
+Controller.list = function (req, res) {
+    var random = null;
+    model.list(random, function (error, info) {
+        return res.render('listpage', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+
+// Product Content
+Controller.product_display= function (req, res) {
+    var random = null;
+    model.product_display(random, function (error, info) {
+        return res.render('product_display', {
+            title: 'E-Commerce Website',
+            href:'../../public/product_display.css'
+        });
+    })
+}
+
+// Order Page
+Controller.cart = function (req, res) {
+    var random = null;
+    model.cart(random, function (error, info) {
+        return res.render('cartpage', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+
+// Login/Registration (User)
+Controller.user_login = function (req, res) {
+    var random = null;
+    model.user_login(random, function (error, info) {
+        return res.render('user-login', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+
+// Login/Registration (Seller)
+Controller.seller_login = function (req, res) {
+    var random = null;
+    model.seller_login(random, function (error, info) {
+        return res.render('seller-login', {
+           
+        });
+    })
+}
+
+// User Profile
+Controller.user_profile = function (req, res) {
+    var random = null;
+    model.user_profile(random, function (error, info) {
+        return res.render('user-profile', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+
+// Seller Profile
+Controller.seller_profile = function (req, res) {
+    var random = null;
+    model.seller_profile(random, function (error, info) {
+        return res.render('seller_profile', {
+             title: 'seller_profile',
+             href:'../../public/seller_profile.css'
+        });
+    })
+}
+
+// Order History
+Controller.order_history = function (req, res) {
+    var random = null;
+    model.order_history(random, function (error, info) {
+        return res.render('order-history', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+
+// Address Form
+Controller.add_form = function (req, res) {
+    var random = null;
+    model.add_form(random, function (error, info) {
+        return res.render('address-form', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+
+// Product Form
+Controller.product_form = function (req, res) {
+    var random = null;
+    model.product_form(random, function (error, info) {
+        return res.render('product-list-form', {
+            title: 'E-Commerce Website',
+        });
+    })
+}
+// Product Registration Form
+Controller.product_registration = function (req, res) {
+    var random = null;
+    model.product_form(random, function (error, info) {
+        return res.render('product_registration', {
+            title: 'Product registration',
+           href:"../../public/product_registration.css"
+        });
+    })
+}
+
+module.exports = Controller;
