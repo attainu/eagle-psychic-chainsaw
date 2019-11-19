08175b4a0acc07e27756d8f3264f0f757e868ae5
SITANSHU KUMAR <41771170+sitanshu-kumar-vst-au4@users.noreply.github.com>
2019-11-19 17:33:34 +0530

    seller session

--- index.js
+++ index.js
@@ -1,75 +1,85 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const PORT = 8086;
-const db=require('./models/index.js');
-
+const db = require("./models/index.js");
+const session = require("express-session");
+var cookieParser = require("cookie-parser");
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded());
-app.use('/public', express.static('public'));
-const hbs = exphbs.create({
-    extname: '.hbs'
+app.use(
+  session({
+    resave: false,
+    saveUnintialized: true,
+    secret: "sitanshu123",
+    cookie: {
+      httpOnly: true,
+      maxAge: 120000,
+      path: "/",
+      sameSite: true,
+      secure: false
     }
-)
-app.engine('.hbs', hbs.engine);
-app.set('view engine', '.hbs');
-const controllers=require('./controllers/index.js');
-app.post('/seller',controllers.SellerController.create);
-app.delete('/seller',controllers.SellerController.delete);
-app.post('/seller_signin',controllers.SellerController.signin);
- 
+  })
+);
+app.use("/public", express.static("public"));
+const hbs = exphbs.create({
+  extname: ".hbs"
+});
+app.engine(".hbs", hbs.engine);
+app.set("view engine", ".hbs");
+const controllers = require("./controllers/index.js");
+app.post("/seller", controllers.SellerController.create);
+app.delete("/seller", controllers.SellerController.delete);
+app.post("/seller_signin", controllers.SellerController.signin);
 
-
-
-var pageController = require('./controllers/e-commerce.js');
+var pageController = require("./controllers/e-commerce.js");
 
 // Home route
 //app.get('/', pageController.home)
 
 // Product List
-app.get('/', pageController.home)
+app.get("/", pageController.home);
 
 // Product Content
-app.get('/product_display', pageController.product_display)
+app.get("/product_display", pageController.product_display);
 
 // Order Page
-app.get('/cart', pageController.cart)
+app.get("/cart", pageController.cart);
 
 // Login/Registration (User)
-app.get('/user-login', pageController.user_login)
+app.get("/user-login", pageController.user_login);
 
 // Login/Registration (Seller)
-app.get('/seller-login', pageController.seller_login)
+
+app.get("/seller-login", pageController.seller_login);
 
 // User Profile
-app.get('/user-profile', pageController.user_profile)
+app.get("/user-profile", pageController.user_profile);
 
- 
-app.get('/seller_profile', pageController.seller_profile)
+app.get("/seller_profile", pageController.seller_profile);
 
 // Order History
-app.get('/order-history', pageController.order_history)
+app.get("/order-history", pageController.order_history);
 
 // Address From Page
-app.get('/address-form', pageController.add_form)
+app.get("/address-form", pageController.add_form);
 
 // Product List Form Page
-app.get('/product-list-form', pageController.product_form)
+app.get("/product-list-form", pageController.product_form);
 
-app.get('/product_registration', pageController.product_registration)
- 
+app.get("/product_registration", pageController.product_registration);
+
 db.connect()
-.then (function(){
-	app.listen(PORT, function () {
-    console.log("Started : ", PORT);
-}).on('error', function (error) {
-    console.log("Unable To Start App >>>",error);
-});
-})
-.catch(function(error)
-{
-	console.log("Failed to connect with database");
-}) 
-
-
- 
+  .then(function() {
+    app
+      .listen(PORT, function() {
+        console.log("Started : ", PORT);
+      })
+      .on("error", function(error) {
+        console.log("Unable To Start App >>>", error);
+      });
+  })
+  .catch(function(error) {
+    console.log("Failed to connect with database");
+  });
