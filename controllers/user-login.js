85e4840f24b69a2ecce2dbebe6a282073e2516a4
salman-ansari-vst-au4 <salmanahmed1497@gmail.com>
2019-11-21 01:49:04 +0530

    Get and Post of user address

--- controllers/user-login.js
+++ controllers/user-login.js
@@ -1,5 +1,5 @@
 const Controller = {};
-
+const async = require('async');
 var Users = require('./../models/Users-db');
 const Product = require('../models/Products');
 
@@ -74,7 +74,7 @@ Controller.user_delete = function (req, res) {
 // Update User
 Controller.user_update = function (req, res) {
     var data = req.body;
-    Users.register.updateOne({ username: data.username }, { $set: data }, { multi: true, new: true }, function (err, user) {
+    Users.register.updateOne({ password: data.password }, { $set: data }, { multi: true, new: true }, function (err, user) {
         if (err) {
             return res.status(500).send(err);
         }
@@ -127,12 +127,86 @@ Controller.validate = function (req, res, next) {
 
     Users.verify(req, function (error, info) {
         if (error) {
-            console.log("again");
             return next();
         }
         return res.redirect('/user-login');
     });
 }
+
+
+// get user data Address
+Controller.address_get = function (req, res) {
+    var user = req.session.user;
+    Users.register.findOne({
+        username: user.username
+    })
+        .populate('users')
+        .exec(function (err, docs) {
+            var i = 0;
+            console.log("docs>>>", docs)
+            var iter = function (user, callback) {
+                console.log("user>>>", user, ++i)
+                Users.address.populate(user, {
+                    path: 'address'
+                },
+                    callback);
+            };
+
+            async.each(docs, iter, function done(err) {
+
+                res.send({
+                    1: docs,
+                    i: i
+                });
+            });
+        });
+}
+
+// Add Address
+Controller.address_add = function (req, res) {
+    const address = new Users.address({
+        name: req.body.name,
+        number: req.body.number,
+        temp_number: req.body.temp_number,
+        user_address: req.body.user_address,
+        pincode: req.body.pincode,
+        locality: req.body.locality,
+        city: req.body.city,
+        state: req.body.state,
+        landmark: req.body.landmark,
+        add_type: req.body.add_type,
+    })
+    address
+        .save()
+        .then(result => {
+            console.log("1")
+
+            console.log(req.session.user.password)
+            Users.register.updateOne({ username: req.session.user.username },
+                { $push: { address: result._id } },
+                { multi: true, new: true },
+                function (err, user) {
+                    if (err) {
+                        console.log(err);
+                    }
+                    if (!user) {
+                        console.log("No user found");
+                    }
+
+                    console.log(user);
+                })
+            return res.status(200).json({
+                msg: result,
+                id: result._id
+            })
+        })
+        .catch(err => {
+            return res.status(500).json({
+                msg: err
+            })
+        })
+}
+
 
 
 module.exports = {
